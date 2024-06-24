aa01567fb29560aad92257a73e1334485ea4617f
Michal Sojka <michal.sojka@cvut.cz>
2024-06-24 17:14:38 +0200

    Install nix in fastmac

--- script-mac.sh
+++ script-mac.sh
@@ -1,3 +1,6 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
+
+sh <(curl -L https://nixos.org/nix/install) --yes
+
 echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
