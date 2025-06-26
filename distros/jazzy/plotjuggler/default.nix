bb910076bf4f4939046ee914b97c9356c3eac7a1
Michal Sojka <michal.sojka@cvut.cz>
2025-06-26 18:04:49 +0200

    regenerate all distros, Thu Jun 26 18:04:49 2025

--- distros/jazzy/plotjuggler/default.nix
+++ distros/jazzy/plotjuggler/default.nix
@@ -2,20 +2,20 @@
 # Copyright 2025 Open Source Robotics Foundation
 # Distributed under the terms of the BSD license
 
-{ lib, buildRosPackage, fetchurl, ament-cmake, ament-index-cpp, binutils, boost, cppzmq, fastcdr, protobuf, qt5, rclcpp }:
+{ lib, buildRosPackage, fetchurl, ament-cmake, ament-index-cpp, binutils, boost, cppzmq, fastcdr, lua, lz4, mosquitto, nlohmann_json, protobuf, qt5, rclcpp, zstd }:
 buildRosPackage {
   pname = "ros-jazzy-plotjuggler";
-  version = "3.10.1-r1";
+  version = "3.10.7-r1";
 
   src = fetchurl {
-    url = "https://github.com/ros2-gbp/plotjuggler-release/archive/release/jazzy/plotjuggler/3.10.1-1.tar.gz";
-    name = "3.10.1-1.tar.gz";
-    sha256 = "22265917ba2443c985971f3ab39458d5abdd466d94d62aeb1e66b7dc065f4377";
+    url = "https://github.com/ros2-gbp/plotjuggler-release/archive/release/jazzy/plotjuggler/3.10.7-1.tar.gz";
+    name = "3.10.7-1.tar.gz";
+    sha256 = "e77e4a36fa28b2a75a8bc10f51abf1e3bc8caca1c2e712fd44e0a7e8c4f91f92";
   };
 
   buildType = "ament_cmake";
   buildInputs = [ ament-cmake ];
-  propagatedBuildInputs = [ ament-index-cpp binutils boost cppzmq fastcdr protobuf qt5.qtbase qt5.qtsvg qt5.qtwebsockets qt5.qtx11extras rclcpp ];
+  propagatedBuildInputs = [ ament-index-cpp binutils boost cppzmq fastcdr lua lz4 mosquitto nlohmann_json protobuf qt5.qtbase qt5.qtsvg qt5.qtwebsockets qt5.qtx11extras rclcpp zstd ];
   nativeBuildInputs = [ ament-cmake ];
 
   meta = {
