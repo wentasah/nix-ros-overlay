afc2db2ea84c186081bdb4f04c2be7cf2cb17f8f
Michal Sojka <michal.sojka@cvut.cz>
2026-03-22 09:31:04 +0100

    Remove unneeded onetbb alias

--- distros/distro-overlay.nix
+++ distros/distro-overlay.nix
@@ -179,9 +179,6 @@ let
       '';
     });
 
-    # TODO: Remove once onetbb appears in the locked nixpkgs version.
-    onetbb = self.tbb_2022;
-
     rtabmap-viz = rosSuper.rtabmap-viz.overrideAttrs ({
       postFixup ? "", ...
     }: {
