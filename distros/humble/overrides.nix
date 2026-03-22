5d02e0699536a85d9915629d7f5a87ac5ec32804
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-03-22 15:51:57 +0100

    humble.zmqpp-vendor: fix for CMake v4

--- distros/humble/overrides.nix
+++ distros/humble/overrides.nix
@@ -825,5 +825,7 @@ in with lib; {
     '';
   });
 
-  zmqpp-vendor = lib.patchAmentVendorGit rosSuper.zmqpp-vendor { };
+  zmqpp-vendor = lib.patchAmentVendorGit rosSuper.zmqpp-vendor {
+    patchesFor.zmqpp_vendor = [ ./zmqpp-vendor/cmake-version.patch ];
+  };
 }
