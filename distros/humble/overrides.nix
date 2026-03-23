88ea5c6857b31fe581db34a3160078b49cea90df
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-03-23 01:37:00 +0100

    humble.mrt-cmake-modules: fix for tbb2022 used in autoware-map-height-filter

--- distros/humble/overrides.nix
+++ distros/humble/overrides.nix
@@ -464,6 +464,19 @@ in with lib; {
     '';
   });
 
+  mrt-cmake-modules = rosSuper.mrt-cmake-modules.overrideAttrs ({
+    patches ? [], ...
+  }: {
+    # fix FindTBB used in autoware-map-height-fitter
+    # ref. https://github.com/KIT-MRT/mrt_cmake_modules/pull/40
+    patches = patches ++ [
+      (self.fetchpatch2 {
+        url = "https://github.com/KIT-MRT/mrt_cmake_modules/commit/56bb3808fd7883c1afb216bb9b974fb4d6f16ed0.patch";
+        hash = "sha256-p/TMEttj8dIdKAXvvw4P4BTyQKMKwz++CuyGAYdVqJ8=";
+      })
+    ];
+  });
+
   nav2-behaviors = rosSuper.nav2-behaviors.overrideAttrs({
     ...
   }: {
