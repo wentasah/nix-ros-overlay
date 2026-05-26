ecf577c53eb69f92763a1711198081941ae56cb5
thomasung <tomkimsour@hotmail.fr>
2026-05-26 10:03:28 +0200

    distros.build-env : Adds a third code path to read the package list from $NIX_ATTRS_JSON_FILE

--- distros/build-env/setup-hook-builder.pl
+++ distros/build-env/setup-hook-builder.pl
@@ -5,14 +5,21 @@ use JSON::PP;
 my $out = $ENV{"out"};
 
 # Read packages list.
-my $pkgs;
+my @pkgsData;
 
-if (exists $ENV{"pkgsPath"}) {
-    open FILE, $ENV{"pkgsPath"};
-    $pkgs = <FILE>;
+if ($ENV{"NIX_ATTRS_JSON_FILE"} // "") {
+    open FILE, $ENV{"NIX_ATTRS_JSON_FILE"} or die "cannot open $ENV{NIX_ATTRS_JSON_FILE}: $!";
+    my $json_text = do { local $/; <FILE> };
+    my $attrsRef = decode_json $json_text;
     close FILE;
+    @pkgsData = @{$attrsRef->{"chosenOutputs"} or die "chosenOutputs missing from $ENV{NIX_ATTRS_JSON_FILE}"};
+} elsif (exists $ENV{"pkgsPath"}) {
+    open FILE, $ENV{"pkgsPath"};
+    my $pkgs = <FILE>;
+    close FILE;
+    @pkgsData = @{decode_json $pkgs};
 } else {
-    $pkgs = $ENV{"pkgs"}
+    @pkgsData = @{decode_json $ENV{"pkgs"}};
 }
 
 # Create a setup hook that sources the setup hooks of all packages in the
@@ -20,7 +27,7 @@ if (exists $ENV{"pkgsPath"}) {
 mkdir "$out/nix-support" unless -d "$out/nix-support";
 open(my $setupHook, '>', "$out/nix-support/setup-hook");
 
-for my $pkg (@{decode_json $pkgs}) {
+for my $pkg (@pkgsData) {
     for my $path (@{$pkg->{paths}}) {
         my $pathSetupHook = "$path/nix-support/setup-hook";
         print $setupHook ". $pathSetupHook\n" if -e $pathSetupHook;
