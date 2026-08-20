007b762efc106e16bdd29c73112c0f847dbd990b
Michal Sojka <michal.sojka@cvut.cz>
2026-08-20 16:03:02 +0200

    superflore: Try modernized version
    This tests the version from
    https://github.com/ros-infrastructure/superflore/pull/329.

--- pkgs/superflore/default.nix
+++ pkgs/superflore/default.nix
@@ -1,23 +1,28 @@
 { lib, buildPythonPackage, fetchFromGitHub, xmltodict, termcolor, setuptools
 , rosinstall-generator, rosdistro, rosdep, gitpython, requests, docker, pyyaml
-, pygithub, catkin-pkg, rospkg
+, pygithub, catkin-pkg, rospkg, setuptools-scm
 }:
 
 buildPythonPackage rec {
   pname = "superflore";
-  version = "0.3.3-unstable-2026-05-05";
+  version = "0.3.3-unstable-2026-08-20";
 
   src = fetchFromGitHub {
     owner = "wentasah";
     repo = "superflore";
-    # ref = "refs/heads/nixos";
-    rev = "9026b622efaa239211dd3d335bf2de251a5ea625";
-    hash = "sha256-NAFlWY+rQCTr5f5lVIkcdO/LJzZ+hWkFT4YHjTnC6ls=";
-    # date = "2026-05-05T16:26:51+02:00";
+    # ref = "refs/heads/modernize-test-nixos";
+    rev = "17e0fe7b9156249243662ae3d4d5e02997881e55";
+    hash = "sha256-nT+P3cWTLyBtxQDzwJ6JQfSIFz0ziV9v3ZQY25pPRt8=";
+    # date = "2026-08-20T15:50:24+02:00";
   };
 
   pyproject = true;
-  build-system = [ setuptools ];
+  build-system = [ setuptools  setuptools-scm ];
+
+  # version contains "-unstable-<date>" which is not a valid PEP 440
+  # version, so setuptools-scm's pretend-version hook (which would
+  # otherwise just use `version` verbatim) chokes on it.
+  env.SETUPTOOLS_SCM_PRETEND_VERSION = builtins.elemAt (builtins.split "-" version) 0;
 
   propagatedBuildInputs = [
     xmltodict
